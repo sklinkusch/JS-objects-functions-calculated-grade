fac7d51e960c2f76c0f3b13d579d44c1c33ae3c2
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-28 15:28:48 +0100

    wrote original array in js

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+const students = [
+  {
+    name: "Peter",
+    points: 35
+  },
+  {
+    name: "Paul",
+    points: 100
+  },
+  {
+    name: "John",
+    points: 45
+  },
+  {
+    name: "Mary",
+    points: 84
+  },
+  {
+    name: "Jane",
+    points: 15
+  },
+  {
+    name: "Jennifer",
+    points: 92
+  },
+  {
+    name: "Martin",
+    points: 9
+  }
+];
